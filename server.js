674bf84409414b4ae94e73d682082b8afa83b94f
tsztin0217 <tsztin0217@gmail.com>
2025-03-08 00:01:31 -0500

    created index page for viewing all entries and added link on index page to access it

--- server.js
+++ server.js
@@ -1,21 +1,36 @@
 const dotenv = require("dotenv"); // require package
-dotenv.config(); // Loads the environment variables from .env file
+dotenv.config(); // loads the environment variables from .env file
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on("connected", () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-  });
-  
+});
+
+app.use(express.urlencoded({ extended: false })); // body parser middleware
 const Entry = require("./models/entry.js");
 
-app.get("/", async(req, res) => {
+app.get("/", async (req, res) => {
+    const allEntries = await Entry.find();
     res.render("index.ejs");
 })
 
+app.get("/entries", async (req, res) => {
+    const allEntries = await Entry.find();
+    res.render("entries/index.ejs", {
+        entries: allEntries}
+    );
+});
+
 app.get("/entries/new", (req, res) => {
     res.render("entries/new.ejs");
+});
+
+app.post("/entries", async(req, res) => {
+    console.log(req.body);
+    await Entry.create(req.body);
+    res.redirect("/entries");
 });
 
 app.listen(3000, () => {
