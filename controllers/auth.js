0054a4a2d155baf20d59f0ce433cdd7e763d6df6
tsztin0217 <tsztin0217@gmail.com>
2025-03-10 21:34:37 -0400

    Added sign-up post route and sign-up page template

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,3 +6,31 @@ module.exports = router;
 router.get("/sign-up", (req, res) => {
     res.render("auth/sign-up.ejs");
 });
+
+router.post("/sign-up", async (req, res) => {
+    const userInDatabase = await User.findOne({
+        username: req.body.username
+    });
+    if (userInDatabase) {
+        return res.send("Username already taken.");
+    }
+    if (req.body.password != req.body.confirmPassword) {
+        return res.send("Password and Confirm Password must match");
+    }
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+
+    req.body.password = hashedPassword;
+
+
+    const user = await User.create(req.body);
+    res.send(`Thanks for signing up, ${user.username}!`);
+
+    req.session.user = {
+        username: user.username
+    };
+
+    req.session.save(() => {
+        res.redirect("/");
+    });
+
+});
